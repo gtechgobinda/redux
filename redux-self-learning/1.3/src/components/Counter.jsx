48fb9dea1f25bf4a78688eb4c9e9e6dc5420513e
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-04-29 08:55:40 +0530

    some part completed

---
+++ redux-self-learning/1.3/src/components/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Counter({ id, increment, count,decrement }) {
+  return (
+    <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+      <div className="text-2xl font-semibold">{count}</div>
+      <div className="flex space-x-3">
+        <button
+          className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+          onClick={() => increment(id)}
+        >
+          Increment
+        </button>
+        <button className="bg-red-400 text-white px-3 py-2 rounded shadow"
+        onClick={()=>decrement(id)}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
